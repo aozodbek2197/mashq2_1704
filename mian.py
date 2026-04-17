6120dec22ad9d0be64a2faff6407f3de305e0a92
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-17 12:53:03 +0500

    Create mian.py

---
+++ mian.py
@@ -0,0 +1,19 @@
+# 1-mashq
+t = (10, 20, 30)
+print(t[1])
+# 2-mashq
+nums = [1, 2]
+nums.append(3)
+print(nums)
+# 3-mashq
+nums = [1, 2, 3]
+nums.remove(2)
+print(nums)
+# 4-mashq
+t = (1, 2, 3)
+lst = list(t)
+print(lst)
+# 5-mashq
+lst = [1, 2, 3]
+t = tuple(lst)
+print(t)
